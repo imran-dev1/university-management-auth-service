2281883a149761cb2359aa2072ad02ef1294ec41
webdev-it <webdevit3@gmail.com>
2023-06-07 14:10:13 +0600

    Setup eslint prettier husky lint-staged

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,11 @@ import cors from 'cors'
 const app: Application = express()
 
 //Middlewares
-                          app.use(cors())
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res: any) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to the University Management Server!')
 })
 
