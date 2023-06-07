fb85a7b94280850bbeec91350a51d906d5a5e3cf
webdev-it <webdevit3@gmail.com>
2023-06-07 13:21:10 +0600

    testing

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from 'cors'
 const app: Application = express()
 
 //Middlewares
-app.use(cors())
+                          app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
